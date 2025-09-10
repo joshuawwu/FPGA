d4ef006ff792794fe78f10120ca9b553eb3512d6
Joshua <joshuawu158@gmail.com>
2025-09-10 01:02:16 -0500

    finished rtds

--- Beowulf/RTDS.sv
+++ Beowulf/RTDS.sv
@@ -1,5 +1,6 @@
 module RTDS #(
     parameter BRAKE_THRESHOLD = 12'd2
+    parameter SOUND_DURATION = 12'd3000  // Duration for which the sound should play (in clock cycles)
 ) (
     input logic clk,
     input logic rst,
@@ -10,14 +11,69 @@ module RTDS #(
 
     output logic brake_light,       // active low
     output logic speaker,           // active low
-    output logic ready_to_drive    // active high
+    output logic ready_to_drive     // active high
 );
 
+logic [11:0] sound_counter;
+
+typedef enum logic [2:0] {          // i fuck with one hot
+    DEFAULT = 3'b001,
+    SOUND = 3'b010,
+    DRIVE = 3'b100
+} state_t;
+
+state_t current_state, next_state;
+
+// state FSM
+always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+        current_state <= DEFAULT;
+        sound_counter <= 12b'0;
+    end else begin  
+        current_state <= next_state;
+        if (current_state == SOUND) begin
+            sound_counter <= sound_counter + 1;
+        end else begin
+            sound_counter <= 12'b0;
+        end
+    end
+end
+
+// main logic
+always_comb begin
+    // default
+    next_state = current_state;
+    speaker = 1'b1;
+    ready_to_drive = 1'b0;
+
+    case (current_state)
+        DEFAULT: begin
+            if (BSE > BRAKE_THRESHOLD && start_button == 1'b1 && SDC_final == 1'b1) begin
+                next_state = SOUND;
+            end
+        end 
+        SOUND: begin
+            speaker = 1'b0;
+            // ready_to_drive = 1'b1;       // whether or not we do this here is debateable
+            if (sound_counter > SOUND_DURATION) begin
+                next_state = DRIVE;
+            end            
+        end
+        DRIVE: begin
+            ready_to_drive = 1'b1;
+            if (SDC_final == 0'b0) begin
+                next_state = DEFAULT;
+            end
+        end
+    endcase
+end
+
+// brake light
 always_comb begin
     if (BSE > BRAKE_THRESHOLD) begin
-        brake_light = 1'b0; // Turn on brake light
+        brake_light = 1'b0;                 // Turn on brake light
     end else begin
-        brake_light = 1'b1; // Turn off brake light
+        brake_light = 1'b1;                 // Turn off brake light
     end
 end
     
